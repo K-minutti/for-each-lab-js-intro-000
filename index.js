40082fa184ca283e919cdf00b38621e623abeba9
Kevin Minutti <minutti70@gmail.com>
2020-03-24 01:34:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array) {
+  array.forEach(element => (${index}: ${element}))
+}
+
+function iterate(callback) {
+  var array = [1, 2, 3, 4]
+  array.forEach(element)
+  console.log(element) // we have to return the array
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
